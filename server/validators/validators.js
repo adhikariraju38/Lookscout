9ddaa7ba92763d5b15ec37e116acc833ccffe467
Raju kumar Yadav <notrajuyadav@gmail.com>
2024-05-01 07:49:58 +0545

    Frontend completed

--- server/validators/validators.js
+++
@@ -1,9 +0,0 @@
-const { check } = require("express-validator");
-
-exports.contactValidator = [
-  check("name").notEmpty().withMessage("Name field must not be empty"),
-  check("email").isEmail().withMessage("Invalid email"),
-  check("message").notEmpty().withMessage("Message field must not be empty"),
-  
-];
-
